85730e9927f52b661823d71915f09f267ebd15dc
Perezented <57721734+Perezented@users.noreply.github.com>
2020-01-30 16:31:28 -0600

    finished everything but the car distance part.

--- index.js
+++ index.js
@@ -237,7 +237,19 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {}
+class ProjectManager extends Instructor {
+    constructor(lambdaSian) {
+        super(lambdaSian);
+        (this.gradClassName = lambdaSian.gradClassName),
+            (this.favInstructor = lambdaSian.favInstructor);
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
 
 /*
   STRETCH PROBLEM (no tests!)
